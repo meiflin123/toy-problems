2e90e6754a41f41eb2bea75fd13d0d98b67b347a
Meifang Lin <meiflin123@gmail.com>
2019-06-11 20:10:34 -0700

    upload all toy problems

---
+++ htmlJQueryColor.js
@@ -0,0 +1,22 @@
+
+
+ /*Write a function that wraps every word in its own <span> tag.
+ Make each word change color once per second to a random color.*/
+
+
+ $(function() {
+  // --------------STEP 1--------------
+  // wrap every word in every `<p>` tag with a `<span>` tag.
+  // for example: <p>Hey there</p>
+  // becomes: <p><span>Hey</span><span>there</span></p>
+  // HINT: the `split` array method is your friend
+
+  // TODO: your code here!
+
+
+  // --------------STEP 2--------------
+  // Next, change spans to random colors, once per second
+
+  // TODO: your code here!
+
+});
