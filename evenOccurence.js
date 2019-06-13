fa336335b0e4fbfc015658f06876367bed9808f9
Meifang Lin <meiflin123@gmail.com>
2019-06-12 17:06:05 -0700

    complete evenOccurance

--- evenOccurence.js
+++ evenOccurence.js
@@ -10,6 +10,37 @@
  * console.log(onlyEven); //  4
 */
 
-var evenOccurrence = function(arr) {
-  // Your code here.
+let evenOccurrence = function(arr) {
+  // create hash table
+  // loop arr
+    // store each item into hash table, keep count 
+  // loop arr
+    // if current item in hash table is even, return item
+  // if nothing, return null
+  
+  let hashTable = {};
+  
+  for (let num of arr) {
+    if (!hashTable.hasOwnProperty(num)) {
+    //if (!hashTable[num]) {
+      hashTable[num] = 1
+    } else {
+      hashTable[num] +=1
+    }
+  }
+  
+  
+  
+  for (let num of arr) {
+    let current = hashTable[num]
+    if (current % 2 === 0) {return num}
+  }
+  
+ 
+  return null
+                
+
 };
+
+console.log(evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]))
+
