517ebe165261a54f350095e05b0c265515d96ee4
Meifang Lin <meiflin123@gmail.com>
2019-06-13 16:39:04 -0700

    complete firstNonRepeatedCharacter

--- nonrepeatedCharacter.js
+++ nonrepeatedCharacter.js
@@ -6,6 +6,25 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+let firstNonRepeatedCharacter = function(string) {
+  // loop through string, add ch to table with value of true, if existed, value false.
+  // loop through string, iterate eah ch, if ch in hash table is true, return ch
+  // if nothing, return false
+
+  let hashTable = {};
+  for (let i of string) {
+    if (!hashTable[i]) {
+      hashTable[i] = 'T';
+    } else {
+      hashTable[i] = 'F'
+    }
+  }
+
+  for (let i of string) {
+    if (hashTable[i] === 'T') {
+      return i
+    }
+  }
+
+  return null;
 };
