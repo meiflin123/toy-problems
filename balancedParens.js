9f931f57e0c9260434c320681795c7cdd98e5637
Meifang Lin <meiflin123@gmail.com>
2019-06-13 18:08:23 -0700

    complete balancedParens

--- balancedParens.js
+++ balancedParens.js
@@ -23,5 +23,31 @@
  *
  *
  */
-var balancedParens = function(input) {
+let balancedParens = input => {
+  // criteria: 1. opening tags are closed in reverse orders. -- stack
+  //           2. each opening tag has a closing tag.
+  // create hash table of pairs
+  // iterate input, 
+    //if it's open tag, push to  stack.
+    //if closing tag, pop off the last item from stack and check if they are a pair.
+      // if not, return false
+  // check remaining open tags.
+  
+  let stack = [];
+  let pairs = {'{': '}', '[': ']', '(':')'};
+  let parens = ['}', ')', ']']
+
+  for (let item of input) {
+    if (pairs[item]) {
+      stack.push(item)
+    } else if (parens.includes(item)){
+      let lastOpening = stack.pop();
+      if (pairs[lastOpening] !== item) {
+        return false;
+      }
+
+    }
+  }
+  return stack.length === 0? true: false
 };
+
