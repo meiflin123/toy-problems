7d3b9c0cdc59a2800b9ce679f9a80f435c7129bc
Meifang Lin <meiflin123@gmail.com>
2019-06-16 17:32:53 -0700

    complete linkedList

--- linkedList.js
+++ linkedList.js
@@ -20,22 +20,54 @@
 
 
 var LinkedList = function() {
-  //fill me in!
+    this.head = {value: null, next: null};
+    this.tail = {value: null, next: null};
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-) {
+
+LinkedList.prototype.addToTail = function(num) {
+  var node = this.makeNode(num);
+  if (this.tail.value === null) {
+    this.tail = node
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
+  if (this.head.value === null) {
+    
+    this.head = node
+  } 
 };
 
 LinkedList.prototype.removeHead = function() {
+  
+  if (this.head.next === null) {
+    this.head.value = null;
+    return null
+  } 
+  
+  this.head = this.head.next
+
 };
 
-LinkedList.prototype.contains = function(
-) {
+LinkedList.prototype.contains = function(num) {
+  var current = this.head 
+  while (current) {
+    if (current.value === num ) {
+      return console.log(true)
+    } else {
+      current = current.next
+    }
+   
+  
+  }
+  return console.log(false);
+}
+
+LinkedList.prototype.makeNode = function(num) {
+  return { value: num, next: null};
 };
 
-LinkedList.prototype.makeNode = function(
-) {
-};
+var list = new LinkedList();
