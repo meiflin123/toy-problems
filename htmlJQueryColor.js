e15f968186b6e09accfdfff947dfd1de46a3f9ff
Meifang Lin <meiflin123@gmail.com>
2019-07-01 23:45:41 -0700

    complete htmlJQeryColor

--- htmlJQueryColor.js
+++ htmlJQueryColor.js
@@ -11,12 +11,28 @@
   // becomes: <p><span>Hey</span><span>there</span></p>
   // HINT: the `split` array method is your friend
 
-  // TODO: your code here!
-
+  $('p').each(function(index, element) {
+    let $el = $(element);
+    let words = $el.text().split(' ');
+    $el.html('');
+    words.forEach(function(word) {
+      let newSpan = $('<span>').text(word + ' ');
+      $el.append(newSpan)
+    })
+  })
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  setInterval(function() {
+    $('span').each(function(index, elem) {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
 
+      $(elem).css('color', 'rgb(' + r + ',' + g + ',' + b + ')');
+    });
+
+  }, Math.random() * 1000);
 });
