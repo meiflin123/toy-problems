42a9cb344e4a83e0309741a3a3a82fc85d4824a1
Meifang Lin <meiflin123@gmail.com>
2019-08-08 20:41:12 -0700

    refactor codes

--- numberToEnglish.js
+++ numberToEnglish.js
@@ -42,7 +42,7 @@ var numbersToWords = {
   80: 'eighty',
   90: 'ninety',
 };
-var numbersToPlace = {
+var t = {
   10: 'ten',
   100: 'hundred',
   1000: 'thousand',
@@ -52,8 +52,6 @@ var numbersToPlace = {
   1000000000000000: 'quadrillion',
   1000000000000000000: 'quintillion',
 };
-
-var t = ['Hundred', 'Thousand', 'Million', 'Billion']
 
 Number.prototype.toEnglish = function () {
   // return my value as english words
@@ -68,11 +66,12 @@ Number.prototype.toEnglish = function () {
   // join words array to result
   // return result
 
-  if (num === 0) {return 'Zero'}
+  if (num === 0) { return 'Zero'}
   var number = this.toString();
   var threeDigits = [];
   var len = number.length
   var result = []
+  var place = '1'
   
   while (len > 0) {
     threeDigits.push(number.slice(len - 3 > 0? len -3 : 0, len));
@@ -86,41 +85,48 @@ Number.prototype.toEnglish = function () {
     var hundred;
     var decade;
     var singles;
+    
+    if(i > 0) {
+      place+= '000';
+    }
       
 
     if (str.length === 3) {
-     if (str[0] !== '0') {
-      hundred =  numbersToWords[str[0]] + '-' + t[0];
-      word.push(hundred);
-      
-     }
-      str = str.slice(1);
+      if (str[0] !== '0') { 
+        hundred =  numbersToWords[str[0]] + ' ' + 'hundred';
+        word.push(hundred);
+    
+      }
+      str = str.slice(1);   //slice off 1st number
     }
       
-    if (str.length <= 2) {
-        if (+str) {
-            if (str < 21) { 
-              decade = numbersToWords[+str]
-              word.push(decade);    
-            } else {
-              decade = numbersToWords[str[0] + '0'] ;
-              word.push(decade)
-                if (+str[1]) {
-                   singles = numbersToWords[str[1]];
-                    word.push(singles)
-                }
-             } 
-         }
+    
+    if (+str) {      //if remaining str is not 0
+        if (str < 21) {  // if less than 21, add to word
+          decade = numbersToWords[+str]
+          word.push(decade);    
+        } else {
+          decade = numbersToWords[str[0] + '0'] ;  // if larger than 21, add decade
+          word.push(decade)
+            if (+str[1]) { // and if single is not 0, add single
+               singles = numbersToWords[str[1]];
+               word[word.length-1] = word[word.length-1] + '-' + singles
+            }
+         } 
+     }
           
-      }
+      
       
     if (word.length !==0) {
-        if (i > 0) {  // add thousdands or above.
-          word.push(t[i]);
+       // add thousdands or above.
+        if(i > 0) {
+         word.push(t[place]);
         }
+        
         result.unshift(word.join(' '))
     }    
   }
-    
   return result.join(' ');
 };
+
+
