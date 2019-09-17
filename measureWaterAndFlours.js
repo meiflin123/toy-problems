46e822de1f1dcfffb54b269da624487db3e571dc
Meifang Lin <meiflin123@gmail.com>
2019-09-17 15:33:56 -0700

    complete measureWaterAndFlours

---
+++ measureWaterAndFlours.js
@@ -0,0 +1,31 @@
+/*You have 3 bottles, where the first bottle has 8 Liter water in it, the second bottle is can hold 5 liter and is empty, the third bottle can hold 3 liter and is empty.
+You have to use the other two bottle to measure how many water you fill in from one to the other bottle. The result has to be, that two bottles have exact 4 liters of water.*/
+
+
+
+
+b1 = 8L
+b2 = 5L
+b3 = 3L
+
+//Solution in the form of (b1, b2, b3)
+
+8 0 0
+5 0 3
+5 3 0
+2 3 3
+2 5 1
+7 0 1
+7 1 0
+4 1 3
+4 4 0
+
+
+/*The second test was:
+You have 3 mills.
+3 mills produce 3 tons of flour in 3 hours.
+How many tons flour are producing 2 mills in 2 hours.
+*/
+
+
+//solution: 1 mill produce 1 ton in 3 hrs. 2 milss produce 2 tons in 3 hrs. speed = 2/3 per hr. with 2 hr. 2/3 * 2 = 4/3. 
