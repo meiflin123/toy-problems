ab46f4b7beb52b9e797138aa87964c990568e576
Meifang Lin <meiflin123@gmail.com>
2020-02-11 15:07:03 -0800

    refactor addString

--- addStrings.js
+++ addStrings.js
@@ -24,30 +24,12 @@ Please do the solution in either JavaScript or Ruby so that I can understand bet
 
 
 var addString = function(str1, str2){
-  //split strings into array by decimal points
-    //if whole integer, put 0 at index 1. 
 
-  //compare length for each index, 
-    //  unshift 0 in missing spaces to the left for index0. 
-    //  push 0 in missing spaces to the right for index1.
-  // concat with '.'
+  //split by decimal points, 
+  var arr1 = str1.split('.')
+  var arr2 = str2.split('.')
   
-  //set num1, num2, carry, result
-  //iterate from last character
-    //if char is '.', result = '.' + result. continue with next iteration
-    //add num1 and num2 and carry = sum
-    //reset carry = 0 
-    //if sum >= 10, carry = 1, sum = sum - 10;
-    //if sum > 0, trailingZero is false;
-    //if sum === 0 and trailing zero is true, sum = '' and trailingZero = false
-    //result = sum + result;
-
-  //if there's remaining carry, append it to the result.
-  //return result.
-
-  var arr1 = str1.split('.');
-  var arr2 = str2.split('.');
-  
+  //if whole integer, put 0 at index 1. 
   if(arr1.length < 2) {
     arr1.push('0')
   }
@@ -56,68 +38,72 @@ var addString = function(str1, str2){
     arr2.push('0')
   }
 
-  for(var i = 0; i < 2; i++) {
-      if(arr1[i].length > arr2[i].length) {
-      arr2[i] = padLength(arr1[i], arr2[i], i);
-    } else if (arr1[i].length < arr2[i].length){
-      arr1[i] = padLength(arr2[i], arr1[i], i);
-    }
-  }
+ // pad the short with '0';
+ for (var i = 0; i <= 1; i++){
+   if(arr1[i].length > arr2[i].length){
+     arr2[i] = compareLength(arr1[i], arr2[i], i)
+   } else {
+     arr1[i] = compareLength(arr2[i], arr1[i], i)
+   }
+ }
+  // concat with '.'
+  arr1 = arr1.join('.');
+  arr2 = arr2.join('.');
+  
 
-  var num1 = arr1[0] + '.' + arr1[1];
-  var num2 = arr2[0] + '.' + arr2[1];
+  // sum would be numbers at the same index add up, plus possible carry from previous iteration. 
+  let result = '', sum = 0, carry = 0, trailingZero = true;;
+  for(var i = arr1.length -1; i >= 0; i--) {
 
-
-  var int1, int2, sum, carry = 0, result = '', trailingZero = true;
-  for (var i = num1.length-1; i >=0; i--) {
-    if(num1[i] === '.') {
+    if(arr1[i] === '.') {  
+      if (result.length === 0) { 
+        trailingZero = false;
+        continue;
+      }
       result = '.' + result;
       continue;
     }
-
-    int1 = parseInt(num1[i]);
-    int2 = parseInt(num2[i]);
-    sum = int1 + int2 + carry;
+    // sum = num1 + num2 + carry from last iteration. 
+    sum = parseInt(arr1[i]) + parseInt(arr2[i]) + carry;
     carry = 0;
-
+    
+    // if sum >= 10, carry = 1 for next iteration. 
     if(sum >= 10) {
-      carry = 1;
+      carry = 1; 
       sum = sum - 10;
-
-      
-    }
-    if(sum > 0) {
-        trailingZero = false;
     }
 
+    // if sum is nonZero, turn off trailingZero.
+    if(sum !== 0) {
+      trailingZero = false;
+    }
+    
+    // if sum is 0 and yet no nonzeros, sum = ''; 
     if(sum === 0 & trailingZero === true) {
       sum = '';
-      trailingZero = false;
     } 
+
     result = sum + result;
   }
-
-  if(carry !== 0) {
+  if(carry != 0) {
     result = carry + result;
   }
   return result;
 }
 
-var padLength = function(long, short, index) {
-  var zero = ''
-  for (var i = 0; i < long.length - short.length; i++) {
-    zero+=0;
+
+var compareLength = (long, short, idx) => {
+  var zero = '';
+  for(var i = 0; i < long.length - short.length; i++) {
+      zero+= '0'
   }
-  if(index === 0 ) {
-    short = zero + short
-  } else if (index === 1) {
-    short = short + zero
+  if(idx === 0) {
+      short = zero + short;
+  } else {
+      short = short + zero;
   }
   return short;
 }
 
 
-addString( '3.141', '10.090')
-
-addString( '1099.999999999990001', '10000000.000000000000000201000')
-
+addString('120.08', '10.9900')
