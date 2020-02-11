e557ef7d0c5167b4d38c3a5f607dca9704ce7135
Meifang Lin <meiflin123@gmail.com>
2020-02-11 11:59:47 -0800

    modified codes

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -7,60 +7,53 @@
 */
 
 
-let DPlongestPalindrome = string => {
-  //solution 1:dynamic programing
-  //criteria1: ch at i === ch at j 
-  //criteria2: substring inside a palindrome should be palindrome || one single ch 
-  // if found palindrome, compare to length of existing palindrome 
-  // time complexity: O(n^2)
-  // space complexity: O(n^2)
+let longestPalindrome = function (string) {
+  // solution1:
+  // double for loop i and j, i < j
+  // if two characters are the same, slice the substring, check if it's parlindrome and if it's longer than stored parlindrome
+  // output parlindrome. 
 
-  if (string === null || string.length < 1) { return null}
-  if (string.length ===1) {return string};
-  
-  let start = 0;
-  let end = 0; 
-  
-  for (var j = 1; j < string.length; j++) {
-    for (var i = 0; i < j; i++) {
-      if (string[i] === string[j]) {
-        let substring = string.slice(i+1, j);
-        
-        if (isPalindrome(substring) || j-i <3) {
-         
-          if (j-i > end-start) {
-            start = i
-            end = j          
-          }         
-        }   
-      } 
+  // time complexity: O(n^2)
+  // space complexity: O(n^2);
+  if(string.length === 0) { return ''}
+  let result = ''
+  for(let j = 1; j < string.length; j++) {
+    for(let i = 0; i < j; i++) {
+      if(string[i] === string[j]) {
+        let sub = string.slice(i, j+1);
+        if(isPalindrome(sub) && sub.length > result.length) {
+          result = sub;
+        }
+      }
     }
   }
-  
-  return string.slice(start, end+1)
+  return result || string[0];
 };
-  
-let isPalindrome = string => {
-  let reverseStr = string.split('').reverse().join('');
-  return reverseStr === string;
+
+let isPalindrome = (string) => {
+  let newStr = string.split('').reverse().join('')
+  return newStr === string
+
 }
 
 let centeredLongestP = string => {
   // solution 2: expand from center
-  // loop over each ch, 
-    //pass two neighbored ch to expandCenter P. case: noon
-    // pass index -1, index +1 to expandCenter P. case: dad
+  // for loop 
+    // pass i, i+1 ch to expandCenter. 
+      // if they are equal, expand left and right.  case: noon
+    // pass i, i + 2 ch to expandCenter. case: dad
+      // if they are equal, expand left and right.  case: noon
   // time complexity: O(n^2)
   // space complexity: O(1)
   
-  if (string === null) {return null};
-  if (string.length === 1) { return string};
+  
+  if (string.length === 0) { return ''};
   
   let result = ''
   for (let i = 0; i < string.length -1; i++) {
     
     let evenPal = expandCenterP(i, i+1, string)
-    let oddPal = expandCenterP(i-1, i+1, string)
+    let oddPal = expandCenterP(i, i+2, string)
 
     if (evenPal.length > result.length) {
       result = evenPal
@@ -69,13 +62,12 @@ let centeredLongestP = string => {
       result = oddPal
     }
   }  
-  return result
+  return result || string[0]
 }
 
 let expandCenterP = (left, right, string) => {
-  // criteria:  left ch and right ch are equal, 
-    // expand in each direction
-    // new left and new right should be also equal if Palindrome
+  // while left and right are equal, 
+  // expand left and right
   while ( left >= 0 && right < string.length && string[left] === string[right]) {
     left--
     right++        
